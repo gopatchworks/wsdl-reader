03ea4c6b061d19043144d0bd803211f739fe3f57
Adam Goulding <50168563+adam-g-patchworks@users.noreply.github.com>
2023-07-12 16:29:53 +0100

    Update SoapMessage.php
    Add header property

--- src/Model/Definitions/Implementation/Message/SoapMessage.php
+++ src/Model/Definitions/Implementation/Message/SoapMessage.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace Soap\WsdlReader\Model\Definitions\Implementation\Message;
 
+use DOMElement;
 use Soap\WsdlReader\Model\Definitions\BindingUse;
 
 final class SoapMessage implements MessageImplementation
 {
     public function __construct(
         public readonly BindingUse $bindingUse,
+        public readonly ?DOMElement $header = null
     ) {
     }
 }
