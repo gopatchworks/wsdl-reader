6cb74d3858fed4ab8178ab8362329ec9c4f5c75d
Adam Goulding <50168563+adam-g-patchworks@users.noreply.github.com>
2023-07-18 15:40:04 +0100

    Update SoapMessage.php

--- src/Model/Definitions/Implementation/Message/SoapMessage.php
+++ src/Model/Definitions/Implementation/Message/SoapMessage.php
@@ -5,11 +5,13 @@ namespace Soap\WsdlReader\Model\Definitions\Implementation\Message;
 
 use DOMElement;
 use Soap\WsdlReader\Model\Definitions\BindingUse;
+use Soap\WsdlReader\Model\Definitions\QNamed;
 
 final class SoapMessage implements MessageImplementation
 {
     public function __construct(
         public readonly BindingUse $bindingUse,
+        public readonly ?QNamed $namespace = null,
         public readonly ?DOMElement $header = null
     ) {
     }
